1a387720000198f7dda3a68301ed009fa5f70a02
Pimpan Tantivaravong <pimpannt@gmail.com>
2019-10-01 12:09:59 +0700

    create index.html for vue and react

---
+++ single-spa-app/single-spa.config.js
@@ -0,0 +1,17 @@
+//This file is where you register all of the applications that will be part of the main single page app
+
+import { registerApplication, start } from 'single-spa'
+
+registerApplication(
+  'vue', 
+  () => import('./src/vue/vue.app.js'),
+  () => location.pathname === "/react" ? false : true
+);
+
+registerApplication(
+  'react',
+  () => import('./src/react/main.app.js'),
+  () => location.pathname === "/vue"  ? false : true
+);
+
+start();
